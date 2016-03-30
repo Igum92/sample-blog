cccd2ecce9ea78d84cc6d7cab133ac7263455dd2
Marina Igumnova <igum.marina@gmail.com>
2016-03-30 17:17:56 +0300

    Update articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,16 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+
     if @article.valid?
       @article.save
     else
       render action: 'new'
     end
+
   end
+
+  private
 
   def article_params
     params.require(:article).permit(:title, :text)
