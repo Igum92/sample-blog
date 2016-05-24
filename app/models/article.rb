4854aebfdb988e962eb77919891be326ed734794
Marina Igumnova <igum.marina@gmail.com>
2016-05-24 17:09:44 +0300

    Add validation_length_of for article model (text)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true
   validates_length_of :title, maximum: 140
+  validates_length_of :text, maximum: 4000
   validates :text, presence: true
   has_many :comments
 
