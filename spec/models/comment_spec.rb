e5c9d88c4ab5b76fc877046f4f8ff7e742640a10
Marina Igumnova <igum.marina@gmail.com>
2016-05-24 17:40:19 +0300

    Update comments_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,3 +4,15 @@ require "spec_helper"
 describe Comment do
   it { should belong_to :article }
 end
+
+describe "validation_length_of" do
+ it "article without text is a valid" do
+
+    article = create(:article, title: 'Lorem Ipsum', text: 'Bananza', author: 'Lusya')
+
+
+    comments = article.comments.new()
+    expect(comments.valid?).to eq false
+
+  end
+end
