09bf981bf955ae173097ffc2e11afb8bf5d3b628
Marina Igumnova <igum.marina@gmail.com>
2016-05-27 16:13:38 +0300

    Add create_new_article spec for article

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -10,4 +10,11 @@ feature "Article Creation" do
     expect(page).to have_content I18n.t ('articles.new_article')
   
   end
+
+  scenario "allows user to create new article" do
+    visit new_article_path
+    article = create(:article, title: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, nihil primis per ad. Dolore possim intellegam eos at, ad mei quot elitr accommodare. Lorem facilisi at sea. Summo doming maiorum ad has, at est veniam tantas. Et vix tation nusquam, te mea docendi dissentiet adversarium. Qui brute facer delectus in, mea id assum diceret.')
+
+    expect(article.subject).to eq 'Lorem Ipsum'
+  end
 end
